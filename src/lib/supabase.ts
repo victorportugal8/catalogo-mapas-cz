ba9458754ad3bee4feff813aa40c5b1a98a5c5e8
Victor Portugal <victorportugalnobrega@gmail.com>
2026-09-24 15:10:22 -0300

    Estabelecendo a conexão com o BD

---
+++ src/lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Faltam as variáveis de ambiente do Supabase.')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
